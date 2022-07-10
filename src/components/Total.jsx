4ca4844388de0714359748379d771124332fe906
Salladoretz <salladoretz@yandex.ru>
2022-07-10 12:06:47 +1000

    discount fix

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -20,12 +20,12 @@ const Total = () => {
             <div className={css.total__item}>
                 <p>Сумма:</p>
                 <h2
-                    className={discount > 0 ? css.total_striked : ''}
+                    className={discount > 0 && count !== 0 ? css.total_striked : ''}
                 >
                     {total}
                 </h2>
             </div>
-            <div className={discount === 0 ? css.total_erase : css.total__item}>
+            <div className={discount === 0 || count === 0 ? css.total_erase : css.total__item}>
                 <p>Сумма со скидкой:</p>
                 <h2>{totalDiscount}</h2>
             </div>
